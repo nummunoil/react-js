84bd60b3504dd13da732f8aefc6eaf9ef3da8ed4
Oil Sudarat <sudarat@enres.co>
2021-03-22 17:13:43 +0700

    Expressions in JSX

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,6 +1,12 @@
 function App() {
-  return <div className="App">
+  const name = 'Oil'
+  const x = false
+
+  return <div className="container">
     <h1>Hello From React</h1>
+    <h2>Hello {name}</h2>
+    <h2>Hello {x ? 'Yes' : 'No'}</h2>
+
   </div>;
 }
 
