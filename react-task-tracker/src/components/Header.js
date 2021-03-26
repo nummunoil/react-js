70dcee088f7e292aa99fc03e0d916ab07e589b87
Oil Sudarat <sudarat@enres.co>
2021-03-26 15:34:44 +0700

    Styling

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -3,17 +3,18 @@ import PropTypes from 'prop-types'
 const Header = ({title}) => {
   return (
     <header>
-      <h1 style={{color:'red', backgroundColor: 'black'}}>{title}</h1>
+      <h1 style={headingStyle}>{title} </h1>
     </header>
   );
 };
 
 Header.defaultProps = {
-  title: 'Task Tracker'
+  title: 'Task Tracker',
 }
 
 Header.propTypes = {
   title: PropTypes.string.isRequired
 }
 
+const headingStyle={color:'red', backgroundColor: 'black'}
 export default Header;
