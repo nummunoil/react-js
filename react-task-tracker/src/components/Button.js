a762870a2767888dc3516e0471ea383c3f023369
Oil Sudarat <sudarat@enres.co>
2021-03-29 18:28:16 +0700

    Events

--- react-task-tracker/src/components/Button.js
+++ react-task-tracker/src/components/Button.js
@@ -1,10 +1,6 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ color, text }) => {
-  const onClick = (e) => {
-    console.log(e);
-  };
-
+const Button = ({ color, text, onClick }) => {
   return (
     <button onClick={onClick} style={{ backgroundColor: color }} className="btn">
       {text}
@@ -19,6 +15,7 @@ Button.defaultProps = {
 Button.propTypes = {
   text: PropTypes.string,
   color: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default Button;
