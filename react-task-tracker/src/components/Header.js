017a898769e6a2a364bf644833ad7e0393fe0b55
Oil Sudarat <sudarat@enres.co>
2021-03-29 18:20:48 +0700

    Button Component

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -5,9 +5,7 @@ const Header = ({ title }) => {
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button color="green" text="Hello" />
-      <Button color="blue" text="Hello 2" />
-      <Button color="red" text="Hello 3" />
+      <Button color="green" text="Add" />
     </header>
   );
 };
