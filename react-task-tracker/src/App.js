71fbab5f6ea403857e6c21f5039b866c8933305f
Oil Sudarat <sudarat@enres.co>
2021-04-05 19:19:54 +0700

    Delete task & prop drilling

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -25,10 +25,15 @@ const App = () => {
     },
   ]);
 
+  //Delete Task
+  const deleteTask = (id) => {
+    console.log('delete : ', id);
+  };
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 };
