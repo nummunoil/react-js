2d0ecee187c8c764685449fdc81ff876a7f51da0
Oil Sudarat <sudarat@enres.co>
2021-04-12 20:32:08 +0700

    Optional message if no tasks

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -33,7 +33,11 @@ const App = () => {
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        'No Tasks To Show'
+      )}
     </div>
   );
 };
