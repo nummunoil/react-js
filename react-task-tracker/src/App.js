43ae0d7187f7d12163f4a638354f1e19246f8de0
Oil Sudarat <sudarat@enres.co>
2021-04-12 20:42:41 +0700

    Toggle reminder & conditional styling

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -30,11 +30,16 @@ const App = () => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    console.log('id : ', id);
+  };
+
   return (
     <div className="container">
       <Header />
       {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         'No Tasks To Show'
       )}
