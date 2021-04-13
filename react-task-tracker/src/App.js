2e20dbe4c9e4403f310c17cef32d4af6f16e02be
Oil Sudarat <sudarat@enres.co>
2021-04-13 22:11:10 +0700

    Add task submit

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -26,6 +26,11 @@ const App = () => {
     },
   ]);
 
+  // Add Task
+  const addTask = (task) => {
+    console.log('task : ', task);
+  };
+
   //Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -43,7 +48,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
